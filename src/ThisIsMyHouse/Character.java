0f8a8038d1130721307ec94040378f08292df7d4
JamesGenesis <JamesGenesis@JamesGenesis-PC>
2014-09-21 21:48:48 +0700

    เดินได้แล้ว

---
+++ src/ThisIsMyHouse/Character.java
@@ -0,0 +1,52 @@
+package ThisIsMyHouse;
+
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
+public class Character {
+	private float x;
+	private float y;
+	private float vx;
+	private float vy;
+	private float vjump;
+	private Image image;
+	public static final int WIDTH = 105;
+	public static final int HEIGHT = 140;
+	
+	public Character(float x, float y , float vx , float vy) throws SlickException {
+	    this.x = x;
+	    this.y = y;
+	    this.vx = vx;
+	    this.vy = vy;
+	    this.vjump = vy;
+	    image = new Image("res/Character/Character0.png");
+	}
+	
+	public void render() {
+		image.draw(x,y);
+	}
+	
+	public void update() {
+		
+	}
+
+	public void MoveLeft() {
+		if(x>0){
+			x -=vx;
+		}
+		else{
+			x =0;
+		}
+	}
+
+	public void MoveRight() {
+		if(x<(GameMain.GAME_WIDTH-WIDTH)){
+			x +=vx;
+		}
+		else{
+			x = GameMain.GAME_WIDTH-WIDTH;
+		}
+	}
+	
+	
+}
--- src/ThisIsMyHouse/GameMain.java
+++ src/ThisIsMyHouse/GameMain.java
@@ -5,13 +5,17 @@ import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
 public class GameMain extends BasicGame {
 	private Image BGImage;
+	private static Character character;
 	public static final int GAME_WIDTH = 800;
 	public static final int GAME_HEIGHT = 600;
-	
+	public static final float G =  (float)1;
+	public static final float Character_JUMP_VY =  (float)22;
+	public static final float Character_MOVE_VX =  (float)5;
 	
 	
 	
@@ -37,19 +41,39 @@ public class GameMain extends BasicGame {
 	@Override
 	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
 		BGImage.draw(0,0);
-		
+		character.render();
 	}
 
 	@Override
 	public void init(GameContainer arg0) throws SlickException {
 		BGImage = new Image("res/BG/BGbedroom.png");
-		
+		character = new Character(GAME_WIDTH/2 - Character.WIDTH/2,GAME_HEIGHT-Character.HEIGHT,Character_MOVE_VX,Character_JUMP_VY);
 	}
 
 	@Override
-	public void update(GameContainer arg0, int arg1) throws SlickException {
-		// TODO Auto-generated method stub
+	public void update(GameContainer container, int delta) throws SlickException {
+		character.update();
+		Input input = container.getInput();
+		updateCharacterMovement(input,delta);
 		
 	}
+	
+	public static void updateCharacterMovement(Input input, int delta){
+	    if (input.isKeyDown(Input.KEY_LEFT)) {
+	        character.MoveLeft();
+	      }
+	    if (input.isKeyDown(Input.KEY_RIGHT)) {
+	        character.MoveRight();
+	      }
+	}
+	
+	
+//	@Override
+//	public void keyPressed(int key, char c) {
+//		// TODO Auto-generated method stub
+//	    if (key == Input.KEY_UP) {
+//	    	character.jump();
+//	    }
+//	}
 
 }
